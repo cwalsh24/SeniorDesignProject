18693107900b6c48a979797d775fe9933334107b
cwalsh24 <cwalsh24@ut.utm.edu>
2023-06-26 10:19:46 -0500

    Bomb fuse sound added

--- SeniorDesignProject/Assets/Scripts/Items/Bomb.cs
+++ SeniorDesignProject/Assets/Scripts/Items/Bomb.cs
@@ -6,9 +6,13 @@ public class Bomb : MonoBehaviour
 {
     [SerializeField] private GameObject explodePrefab;
 
+    //throwing sound effect
+    [SerializeField] private AudioSource Fuse;
+
     // Use in Bomb animation
     public void Explode() {
         GameObject newBomb = Instantiate(explodePrefab, transform.position, transform.rotation);
+        Fuse.Play();
         newBomb.GetComponent<AttackDamage>().isBombExplosion = true;
         Destroy(gameObject);
     }
